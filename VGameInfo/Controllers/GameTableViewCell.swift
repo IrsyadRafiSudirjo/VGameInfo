7b14e650e5633be598260295f20d285af98695b9
MIrsyadRafiSudirjo <irsyadicad1302@gmail.com>
2021-08-10 15:05:57 +0700

    Create Main Feature

---
+++ VGameInfo/Controllers/GameTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  GameTableViewCell.swift
+//  VGameInfo
+//
+//  Created by Muhammad Irsyad Rafi on 09/08/21.
+//
+
+import UIKit
+
+class GameTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ VGameInfo/Model/Game.swift
@@ -0,0 +1,24 @@
+//
+//  Game.swift
+//  VGameInfo
+//
+//  Created by Muhammad Irsyad Rafi on 10/08/21.
+//
+
+import UIKit
+
+
+
+
+struct Game {
+    let id: String
+    let background_image: UIImage
+    let name: String
+    let description: String
+    let rating: String
+    let released: String
+    
+}
+
+
+
